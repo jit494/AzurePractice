0111fbd29e460313116e29f7c23a2a1dadf0e118
Monojit <monojit.debnath.98@gmail.com>
2025-08-13 01:25:55 +0530

    added testing for configuration in app service.

--- AzureWebApi/Program.cs
+++ AzureWebApi/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["ApplicationInsights:ConnectionString"]);
+
+string Greetings = builder.Configuration["Grettings"] ?? "Hello, World!";
 
 var app = builder.Build();
 
@@ -23,7 +26,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -35,6 +38,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapGet("/", () => Greetings).WithName("GetGreetings").WithOpenApi();
 
 app.Run();
 
